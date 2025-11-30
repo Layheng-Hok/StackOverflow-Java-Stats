f13eb904ac96232ae7f4f3bf03b0c3aee660603d
fanxychild <100819203+Layheng-Hok@users.noreply.github.com>
2025-11-30 20:49:17 +0800

    Move data and init Spring Boot project

---
+++ backend/so-java-stats/src/main/java/com/sustech/so_java_stats/SoJavaStatsApplication.java
@@ -0,0 +1,13 @@
+package com.sustech.so_java_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoJavaStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoJavaStatsApplication.class, args);
+	}
+
+}
---
+++ backend/so-java-stats/src/test/java/com/sustech/so_java_stats/SoJavaStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sustech.so_java_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoJavaStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
