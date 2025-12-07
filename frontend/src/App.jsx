ec80679a92be69f9c367ff4269fcfad1acb50924
fanxychild <100819203+Layheng-Hok@users.noreply.github.com>
2025-12-08 00:45:01 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -97,14 +97,11 @@ const App = () => {
         <div className="flex h-14 items-center justify-between px-8">
           
           <div className="mr-4 flex items-center gap-2 font-bold text-xl text-primary">
-            <svg 
-              viewBox="0 0 120 120" 
-              className="h-8 w-8 fill-current text-orange-500"
-              aria-hidden="true"
-            >
-              <path d="M84.4 93.8V70.6h7.7v30.9H22.6V70.6h7.7v23.2z" />
-              <path d="M38.8 68.4l37.8 7.9 1.6-7.6-37.8-7.9-1.6 7.6zm5-18l35 16.3 3.2-7-35-16.4-3.2 7.1zm9.7-17.2l29.7 24.7 4.9-5.9-29.7-24.7-4.9 5.9zm19.2-18.3l-6.2 4.6 23 31 6.2-4.6-23-31zM38 86h38.6v-7.7H38V86z" />
-            </svg>
+            <img 
+              src="/so-logo.svg" 
+              alt="Logo" 
+              className="h-8 w-8"
+            />
              StackOverflow Java Stats
           </div>
 
